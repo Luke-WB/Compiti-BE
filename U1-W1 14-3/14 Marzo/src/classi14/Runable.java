c59b742698e61f57ed7d0ecc3dc7c0a767e4b28f
Luca Scomazzon <scom.luca@gmail.com>
2023-03-22 13:16:09 +0100

    Aggiungo secondo compito

---
+++ U1-W1 14-3/14 Marzo/src/classi14/Runable.java
@@ -0,0 +1,46 @@
+package classi14;
+
+public class Runable {
+    public static void main(String[] args) {
+
+	Rettangolo uno = new Rettangolo(10, 12);
+	Rettangolo due = new Rettangolo(4, 5);
+	Rettangolo.stampaRettangolo(uno);
+	Rettangolo.stampaDueRettangoli(uno, due);
+
+	Sim miaSim = new Sim("+39 320 4409332");
+
+	miaSim.setCreditoDisponibile(10);
+
+	Chiamata chiamata = new Chiamata("+39 339 3234567", 3);
+	miaSim.getListaChiamate()[0] = chiamata;
+
+	chiamata = new Chiamata("+39 335 234556", 5);
+	miaSim.getListaChiamate()[1] = chiamata;
+
+	chiamata = new Chiamata("+39 320 8832445", 7);
+	miaSim.getListaChiamate()[2] = chiamata;
+
+	miaSim.stampaDati();
+
+	Cliente cUno = new Cliente(12335, "banana", "pompelmo", "frutta@gmail.com", "12-12-23");
+
+	Articolo aUno = new Articolo(122222, "spazzola", 12.25, 55);
+
+	/* Carrello carUno = new Carrello(); */
+	/*
+	 * carUno.setCliente(cUno.getCognome());
+	 */ /* carUno.setElencoArticoli(aUno); */
+
+	/* System.out.println(carUno.getElencoArticoli()); */
+
+	System.out.println("i pezzi sono: " + aUno.getNumeroPezziInMagazzino() + " il codice articolo è: "
+		+ aUno.getCodiceArticolo() + " l'articolo in questione è " + aUno.getDescrizioneArticolo()
+		+ " il prezzo dell'articolo è: €" + aUno.getPrezzo());
+	System.out.println("il codice cliente è: " + cUno.getCodiceCliente() + " il nome è: " + cUno.getNome()
+		+ " il cognome è: " + cUno.getCognome() + " la email è:" + cUno.getEmail() + " la datas è: "
+		+ cUno.getDataIscrizione());
+
+    } // PARENTESI MAIN
+
+} // PARENTESI CLASSE
